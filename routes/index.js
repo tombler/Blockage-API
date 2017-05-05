7738d740433e5ea6e85a45073840d192a5e5a6aa
Tom Griffey <tgriffey9389@gmail.com>
2017-05-05 12:16:23 -0700

    Initial commit. Boilerplate generated from Express Generator package.

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require('../queries');
+
+
+router.get('ROUTE', FUNCTION);
+router.get('ROUTE', FUNCTION);
+router.post('ROUTE', FUNCTION);
+router.put('ROUTE', FUNCTION);
+router.delete('ROUTE', FUNCTION);
+
+
+module.exports = router;
