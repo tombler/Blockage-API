f6f4b9975ce1dd48bf7d445793e47a2c4b2ac3e5
Tom Griffey <tgriffey9389@gmail.com>
2017-05-10 17:35:46 -0700

    New routes for auth'ing user, retrieving user apps, and adding an app based on front-end extension development.

--- db.sql
+++ db.sql
@@ -45,33 +45,38 @@ CREATE DATABASE blockage ENCODING 'UTF8';
 CREATE EXTENSION "uuid-ossp";
 
 CREATE TABLE blockage_user (
+    extension_id TEXT PRIMARY KEY
+);
+
+CREATE TABLE tracked_app (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
     name TEXT,
-    email TEXT
-    -- password? Auth token?
+    url TEXT,
+    extension_id TEXT REFERENCES blockage_user (extension_id)
 );
 
-CREATE TABLE blockage_user_app (
-    blockage_user_id UUID,
-    app_id UUID,
-    PRIMARY KEY(blockage_user_id,app_id)
-);
+INSERT INTO blockage_user VALUES
+('31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee');
 
-CREATE TABLE app (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-    name TEXT,
-    web_url TEXT,
-    android_url TEXT,
-    ios_url TEXT,
-    logo TEXT
-);
+INSERT into tracked_app VALUES
+('8ed29b3a-32cb-11e7-9656-77fc695c4cff','facebook.com','Facebook','31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'),
+('950f777a-32cb-11e7-9656-ab6f2e19e18e','mail.google.com','Gmail','31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'),
+('9e008e50-32cb-11e7-9656-f399021b0dab','twitter.com','Twitter','31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee');
 
-CREATE TABLE session (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-    session_start TIMESTAMP,
-    session_end TIMESTAMP,
-    session_time INTERVAL,
-    app_id UUID REFERENCES app (id),
-    blockage_user_id UUID REFERENCES blockage_user (id)
-);
+-- CREATE TABLE blockage_user_app (
+--     blockage_user_id UUID,
+--     app_id UUID,
+--     PRIMARY KEY(blockage_user_id,app_id)
+-- );
+
+
+
+-- CREATE TABLE session (
+--     id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
+--     session_start TIMESTAMP,
+--     session_end TIMESTAMP,
+--     session_time INTERVAL,
+--     app_id UUID REFERENCES app (id),
+--     blockage_user_id UUID REFERENCES blockage_user (id)
+-- );
 
--- routes/index.js
+++ routes/index.js
@@ -3,44 +3,29 @@ var router = express.Router();
 
 var db = require('../queries');
 
-//USERS
-// get single user
-router.get('/api/1.0/user/:id', db.getUser);
-//add user
-router.post('/api/1.0/user', db.createUser);
+// middleware to use for all requests
+router.use(function (req, res, next) {
 
-//APPS
-// Get all apps
-router.get('/api/1.0/app', db.getAllApps);
-// Get single app
-router.get('/api/1.0/app/:id', db.getSingleApp);
-// Create a new app
-router.post('/api/1.0/app', db.createApp);
-// Remove an app
-router.post('/api/1.0/app/:id/delete', db.removeApp);
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://apagkcgippjgcmepobhiifkpccpfljkf');
 
-// // USERs and APPs
-//get all tracked apps for a user
-router.get('/api/1.0/user/:id/app', db.getUserApps);
-// //get single tracked app for a user
-// router.get('/api/1.0/user/:id/app/:id', FUNCTION);
-// Add an existing tracked app for a user
-router.post('/api/1.0/user/:blockage_user_id/app/:app_id', db.userAddApp);
-// // Delete an existing tracked app for a user
-// router.post('/api/1.0/user/:id/app/:id/delete', FUNCTION);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-// // SESSIONs
-// // Get single session - will this ever be necessary?
-// router.get('/api/1.0/session/:id', FUNCTION);
-// Create a new session - is this RESTful?
-router.post('/api/1.0/session', db.storeSession);
-// // Get all sessions for an app
-// router.get('/api/1.0/session/app/:id', FUNCTION);
-// // Get all sessions for a user
-// router.get('/api/1.0/session/user/:id', FUNCTION);
-// // Get all sessions for user + app
-// router.get('/api/1.0/session/user/:blockage_user_id/app/:app_id', FUNCTION);
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 
+router.post('/api/1.0/user', db.checkIfUserExists);
+router.get('/api/1.0/app', db.getApps);
+router.post('/api/1.0/app', db.addApp);
 
 module.exports = router;
