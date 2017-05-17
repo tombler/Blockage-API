a48a30ff2b679b72ab1770d6f64b6732d3cda6c4
Tom Griffey <tgriffey9389@gmail.com>
2017-05-17 16:05:31 -0700

    First draft of analytics queries.

---
+++ analytics_queries.sql
@@ -0,0 +1,36 @@
+-- Default analytics:
+
+--- Number of sessions per app per day, over time (last month)
+select a.id,a.name,a.url,a.extension_id,b.start::date as date,
+COUNT(b.id) as sessions
+from application a
+left join session b on b.extension_id = a.extension_id and b.application_id = a.id
+where a.extension_id = '31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'
+and b.start::date > (select date_trunc('day', NOW() - interval '1 month'))
+group by a.id,a.name,a.url,a.extension_id,b.start::date
+order by a.name,b.start::date;
+
+-- - Total time per app per day, over time (last month)
+select a.id,a.name,a.url,a.extension_id,b.start::date as date,
+SUM(b.duration) as total_time
+from application a
+left join session b on b.extension_id = a.extension_id and b.application_id = a.id
+where a.extension_id = '31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'
+and b.start::date > (select date_trunc('day', NOW() - interval '1 month'))
+group by a.id,a.name,a.url,a.extension_id,b.start::date
+order by a.name,b.start::date;
+
+
+-- With parameters from user:
+
+-- - Window over 24 hrs per app
+select b.id,b.start,b.stop,b.duration
+from application a
+join session b on b.extension_id = a.extension_id and b.application_id = a.id
+where a.extension_id = '31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'
+and a.id = 'ecce86e4-35f6-11e7-80ad-f396424a9a3d'
+and b.start::date = '2017-05-17' --user chooses date param
+order by b.start;
+
+
+
--- db.sql
+++ db.sql
@@ -52,13 +52,14 @@ CREATE TABLE application (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
     name TEXT,
     url TEXT,
-    extension_id TEXT REFERENCES extension (id)
+    extension_id TEXT REFERENCES extension (id),
+    in_session BOOLEAN
 );
 
 CREATE TABLE session (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
-    start TIMESTAMP,
-    stop TIMESTAMP,
+    start TIMESTAMP WITH TIME ZONE,
+    stop TIMESTAMP WITH TIME ZONE,
     duration INTERVAL,
     application_id UUID REFERENCES application (id),
     extension_id TEXT REFERENCES extension (id)
@@ -71,21 +72,3 @@ INSERT into application VALUES
 ('8ed29b3a-32cb-11e7-9656-77fc695c4cff','facebook.com','Facebook','31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'),
 ('950f777a-32cb-11e7-9656-ab6f2e19e18e','mail.google.com','Gmail','31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee'),
 ('9e008e50-32cb-11e7-9656-f399021b0dab','twitter.com','Twitter','31bc99ffd36ddfcb5d350982c31b4f1f96e45c51293ff622b8e0947b9fee');
-
--- CREATE TABLE blockage_user_app (
---     blockage_user_id UUID,
---     app_id UUID,
---     PRIMARY KEY(blockage_user_id,app_id)
--- );
-
-
-
--- CREATE TABLE session (
---     id UUID PRIMARY KEY DEFAULT uuid_generate_v1mc(),
---     session_start TIMESTAMP,
---     session_end TIMESTAMP,
---     session_time INTERVAL,
---     app_id UUID REFERENCES app (id),
---     blockage_user_id UUID REFERENCES blockage_user (id)
--- );
-
